84446d7401508917ecd1e6c2082a2b96e3fc5aee
vedran <vburdzovic@gmail.com>
2021-05-31 20:36:43 -0400

    Update jest, ts-jest, and test files

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
-const { defaults: jestDefaults } = require('jest-config')
-const { defaults: tsjDefaults } = require('ts-jest/presets')
-
 module.exports = {
-  ...jestDefaults,
-  transform: {
-    ...jestDefaults.transform,
-    ...tsjDefaults.transform
-  }
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
--- test/destination.test.ts
+++ test/destination.test.ts
@@ -1,144 +1,39 @@
-import { Stats } from 'webpack'
-import {
-  createDestination,
-  Destination,
-  destinationDefaults,
-  Indefinite,
-  pickDestination
-} from '../src/destination'
+import { Destination, toDestination } from '../src/destination'
 
-const createSource = (options = {}): Indefinite<Partial<Destination>> => ({
-  format: _ => _,
-  requestOptions: {},
-  skip: false,
-  stats: {},
-  url: 'http://a',
-  ...options
-})
+type DestinationKeys = keyof Destination
 
-describe('destinationDefaults', () => {
-  it('has a stats object with source set to false', () => {
-    expect(destinationDefaults.stats.source).toBe(false)
-  })
-})
-
-describe('pickDestination', () => {
+describe('toDestination', () => {
   it('returns undefined if the source object was not provided', () => {
-    expect(pickDestination()).toBeUndefined()
+    expect(toDestination()).toBeUndefined()
   })
 
-  it('returns undefined if the source object is an empty object', () => {
-    const source = {}
+  it('returns undefined if the source object had no url property', () => {
+    const source = { skip: false, statsOptions: {} }
 
-    expect(pickDestination(source)).toBeUndefined()
+    expect(toDestination(source)).toBeUndefined()
   })
 
-  it('returns undefined if the source object had no destination properties', () => {
-    const { format, requestOptions, skip, stats, url, ...rest } = createSource({
-      a: 'a',
-      b: 'b'
-    })
-
-    expect(pickDestination(rest)).toBeUndefined()
-  })
-
-  it("returns an object with the source object's format, requestOptions, skip, stats, and url properties", () => {
-    const source = createSource()
-
-    const properties = ['format', 'requestOptions', 'skip', 'stats', 'url']
-
-    properties.forEach(property => {
-      const item = <Indefinite<Destination>>pickDestination(source)
-
-      expect(item[property]).toBe(source[property])
-    })
-  })
-})
-
-describe('createDestination', () => {
-  it('returns undefined if the source object was not provided', () => {
-    expect(createDestination()).toBeUndefined()
-  })
-
-  it('returns undefined if the source object does not contain a url property', () => {
-    const { url, ...restSource } = createSource()
-
-    expect(createDestination(restSource)).toBeUndefined()
-  })
-
-  it("returns a destination with the source object's format, requestOptions, skip, and url properties", () => {
-    const source = createSource()
-
-    const properties = ['format', 'requestOptions', 'skip', 'url']
-
-    properties.forEach(property => {
-      const destination = <Indefinite<Destination>>createDestination(source)
-
-      expect(destination[property]).toBe(source[property])
-    })
-  })
-
-  it("returns a destination combining the default stats options with the source object's stats options", () => {
-    const source = <{ stats: Stats.ToJsonOptionsObject }>createSource({
-      stats: {
-        assets: false,
-        env: false
-      }
-    })
-
-    const destination = <Destination>createDestination(source)
-
-    expect(destination.stats).toStrictEqual({
-      ...destinationDefaults.stats,
-      ...source.stats
-    })
-  })
-
-  it("returns a destination overwriting its default stats options with the matching source object's stats options", () => {
-    const source = <{ stats: Indefinite<Stats.ToJsonOptionsObject> }>(
-      createSource({
-        stats: {
-          assets: false,
-          env: false,
-          source: true
-        }
-      })
-    )
-
-    type DestinationWithStats = Destination & {
-      stats: Indefinite<Stats.ToJsonOptionsObject>
+  it('returns a destination containing all source object destination fields', () => {
+    const source = {
+      format: () => {},
+      requestOptions: {},
+      skip: false,
+      statsOptions: {},
+      url: 'url'
     }
-    const destination = <DestinationWithStats>createDestination(source)
 
-    Object.keys(destinationDefaults.stats).forEach(key => {
-      expect(destination.stats[key]).toBe(source.stats[key])
+    const item = toDestination(source)
+
+    const keys: DestinationKeys[] = [
+      'format',
+      'requestOptions',
+      'skip',
+      'statsOptions',
+      'url'
+    ]
+
+    keys.forEach(key => {
+      expect(item?.[key]).toBe(source[key])
     })
-  })
-
-  it('returns a destination with the stats preset supplied in the source object', () => {
-    const source = createSource({
-      stats: 'minimal'
-    })
-
-    const destination = <Destination>createDestination(source)
-
-    expect(destination.stats).toBe(source.stats)
-  })
-
-  it("returns a destination with default stats options if the source object's stats property is an empty object", () => {
-    const source = createSource({
-      stats: {}
-    })
-
-    const destination = <Destination>createDestination(source)
-
-    expect(destination.stats).toEqual(destinationDefaults.stats)
-  })
-
-  it('returns a destination with default stats options if the source object does not have a stats property', () => {
-    const { stats, ...restSource } = createSource()
-    const destination = <Destination>createDestination(restSource)
-
-    expect(destination.stats).toEqual(destinationDefaults.stats)
   })
 })
